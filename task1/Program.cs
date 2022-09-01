663d17a9bf8f5a0bca0e4899b52ba35763c7c232
«namesergeyux» <namesergey@gmail.com>
2022-09-01 18:27:34 +0300

    д3 3

---
+++ task1/Program.cs
@@ -0,0 +1,16 @@
+﻿// Задача 1 Напишите программу, которая принимает на вход пятизначное число и проверяет, является ли оно палиндромом.
+
+Console.Write("Введите число из 5 цифр: ");
+string? number = Console.ReadLine();
+
+void Palindrome(string number){
+  if (number[0]==number[4] || number[1]==number[3]) {
+    Console.WriteLine($"Число {number} - палиндром.");
+  }
+  else Console.WriteLine($"Число {number} -  не палиндром.");
+}
+
+if (number!.Length == 5){
+  Palindrome(number);
+}
+else Console.WriteLine($"Не 5 цифр, нужно 5");
---
+++ task2/Program.cs
@@ -0,0 +1,22 @@
+﻿int xa = grafAB("x", "A");
+int ya = grafAB("y", "A");
+int za = grafAB("z", "A");
+int xb = grafAB("x", "B");
+int yb = grafAB("y", "B");
+int zb = grafAB("z", "B");
+
+int grafAB(string coorName, string pointName)
+{
+    Console.Write($"Введите координату {coorName} точки {pointName}: ");
+    return Convert.ToInt16(Console.ReadLine());
+}
+
+double Decision(double xa, double xb, 
+                double ya, double yb, 
+                double za, double zb){
+  return Math.Sqrt(Math.Pow((xb-xa), 2) + Math.Pow((yb-ya), 2) + Math.Pow((zb-za), 2));
+}
+
+double segmentLength =  Math.Round (Decision(xa, xb, ya, yb, za, zb), 2 );
+
+Console.WriteLine($"Длина   {segmentLength}");
